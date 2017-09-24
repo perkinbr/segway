faf0e14799c5856f1f14edec62fc663cf8218d6f
Laurens Valk <laurensvalk@gmail.com>
2017-09-24 18:06:39 +0200

    Tuning

--- ev3/ev3dev/python/parameters.py
+++ ev3/ev3dev/python/parameters.py
@@ -1,22 +1,22 @@
 class Gains:
-    # For every radian (57 degrees) we lean forward,          apply this amount of duty cycle.
+    # For every radian (57 degrees) we lean forward,            apply this amount of duty cycle.
     GyroAngle                  = 6000
     # For every radian/s we fall forward,                       apply this amount of duty cycle.
-    GyroRate                   = 120   
+    GyroRate                   = 95   
     # For every radian we are ahead of the reference,           apply this amount of duty cycle
-    MotorAngle                 = 12
+    MotorAngle                 = 10
     # For every radian/s drive faster than the reference value, apply this amount of duty cycle
-    MotorAngularSpeed          = 2
+    MotorAngularSpeed          = 2.5
     # For every radian x s of accumulated motor angle,          apply this amount of duty cycle
-    MotorAngleErrorAccumulated = 0.1     
+    MotorAngleErrorAccumulated = 2     
 
 
 class Power:
     voltageNominal = 8.0
-    frictionOffsetNominal = 5
+    frictionOffsetNominal = 2
 
 class Timing:
     #Timing settings for the program
-    loopTimeMiliSec             = 15                    # Time of each loop, measured in miliseconds.
-    motorAngleHistoryLength     = 7                     # Number of previous motor angle samples we keep track of.
+    loopTimeMiliSec             = 11                    # Time of each loop, measured in miliseconds.
+    motorAngleHistoryLength     = 5                     # Number of previous motor angle samples we keep track of.
     gyroDriftCompensationFactor = 0.05      
